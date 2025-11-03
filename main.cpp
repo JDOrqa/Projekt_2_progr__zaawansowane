a69ef0592a1e7884cccff46e15f13d275514dd47
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-11-03 21:26:21 +0100

    Implementacja funkcji main dla drzewa BST

--- main.cpp
+++ main.cpp
@@ -24,5 +24,63 @@ int menu() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     return c;
 }
+int main() {
+    BST tree;
+    bool running = true;
+    while (running) {
+        int wybor = menu();
+        switch (wybor) {
+        case 1: {
+            std::cout << "Podaj liczbe calkowita do dodania: ";
+            int v;
+            if (!(std::cin >> v)) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Niepoprawne dane\n";
+            }
+            else {
+                tree.insert(v);
+                std::cout << "Dodano " << v;
+            }
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+        }
+        case 2: {
+            std::cout << "Podaj liczbe do usuniecia: ";
+            int v;
+            if (!(std::cin >> v)) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Niepoprawne dane\n";
+            }
+            else {
+                bool ok = tree.remove(v);
+                if (ok) std::cout << "Usunieto " << v << "!\n";
+                else std::cout << "Nie znaleziono " << v << " w drzewie\n";
+            }
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+        }
+        case 3: {
+            tree.clear();
+            std::cout << "Usunieto cale drzewo\n";
+            break;
+        }
+        case 4: {
+            std::cout << "Inorder: ";
+            tree.printInorder();
+            break;
+        }
+        case 0:
+            running = false;
+            break;
+        default:
+            std::cout << "Nieprawidlowy wybor !\n";
+        }
+    }
+
+    std::cout << "Koniec programu !\n";
+    return 0;
+}
 
 
