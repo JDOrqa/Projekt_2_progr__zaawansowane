24b418fbe904d0a482184ddb7a1cb7ed33b1328f
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-16 19:53:56 +0100

    main zmiana

--- main.cpp
+++ main.cpp
@@ -1,64 +1,64 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <fstream>
 #include "BST.h"
+#include "FileHandler.h"
 
-void waitEnter() {
-    std::cout << "Nacisnij Enter !";
+void pause() {
+    std::cout << "Nacisnij Enter";
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 int menu() {
-    std::cout << "\n---- Proste BST - menu ----\n";
-    std::cout << "1 - Dodaj element\n";
-    std::cout << "2 - Usun element\n";
-    std::cout << "3 - Usun cale drzewo\n";
-    std::cout << "4 - Pokaz inorder \n";
-    std::cout << "0 - Wyjscie\n";
+    std::cout << "\n Menu \n";
+    std::cout << "1. Dodaj element\n";
+    std::cout << "2. Usun element\n";
+    std::cout << "3. Usun cale drzewo\n";
+    std::cout << "4. Szukaj drogi do elementu\n";
+    std::cout << "5. Wyswietl drzewo (preorder/inorder/postorder)\n";
+    std::cout << "6. Wyswietl drzewo (iteracyjne preorder - std::stack)\n";
+    std::cout << "7. Wyswietl drzewo graficznie\n";
+    std::cout << "8. Zapisz drzewo do pliku tekstowego (inorder)\n";
+    std::cout << "9. Zapisz drzewo posortowane (std::sort)\n";
+    std::cout << "10. Zapisz drzewo binarnie (preorder)\n";
+    std::cout << "11. Wczytaj drzewo binarnie\n";
+    std::cout << "12. Wczytaj plik tekstowy z liczbami\n";
+    std::cout << "13. Pokaz size/empty/find\n";
+    std::cout << "0. Wyjscie\n";
     std::cout << "Wybor: ";
     int c;
     if (!(std::cin >> c)) {
         std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.ignore(10000, '\n');
         return -1;
     }
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.ignore(10000, '\n');
     return c;
 }
+
 int main() {
     BST tree;
     bool running = true;
     while (running) {
-        int wybor = menu();
-        switch (wybor) {
+        int choice = menu();
+        switch (choice) {
         case 1: {
             std::cout << "Podaj liczbe calkowita do dodania: ";
             int v;
-            if (!(std::cin >> v)) {
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                std::cout << "Niepoprawne dane\n";
-            }
-            else {
-                tree.insert(v);
-                std::cout << "Dodano " << v;
-            }
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            if (!(std::cin >> v)) { std::cin.clear(); std::cin.ignore(10000, '\n'); break; }
+            tree.insert(v);
+            std::cout << "Dodano " << v << ".\n";
+            std::cin.ignore(10000, '\n');
             break;
         }
         case 2: {
             std::cout << "Podaj liczbe do usuniecia: ";
             int v;
-            if (!(std::cin >> v)) {
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                std::cout << "Niepoprawne dane\n";
-            }
-            else {
-                bool ok = tree.remove(v);
-                if (ok) std::cout << "Usunieto " << v << "!\n";
-                else std::cout << "Nie znaleziono " << v << " w drzewie\n";
-            }
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            if (!(std::cin >> v)) { std::cin.clear(); std::cin.ignore(10000, '\n'); break; }
+            bool ok = tree.erase(v);
+            std::cout << (ok ? "Usunieto\n" : "Nie znaleziono elementu\n");
+            std::cin.ignore(10000, '\n');
             break;
         }
         case 3: {
@@ -67,20 +67,35 @@ int main() {
             break;
         }
         case 4: {
-            std::cout << "Inorder: ";
-            tree.printInorder();
+            std::cout << "Podaj szukany element: ";
+            int v; if (!(std::cin >> v)) { std::cin.clear(); std::cin.ignore(10000, '\n'); break; }
+            auto path = tree.findPath(v);
+            if (path.empty()) std::cout << "Element nie istnieje w drzewie\n";
+            else {
+                std::cout << "Sciezka: ";
+                for (size_t i = 0; i < path.size(); ++i) {
+                    if (i) std::cout << " -> ";
+                    std::cout << path[i];
+                }
+                std::cout << "\n";
+            }
+            std::cin.ignore(10000, '\n');
             break;
         }
-        case 0:
-            running = false;
+        case 5: {
+            std::cout << "Wybierz metode: 1) preorder 2) inorder 3) postorder : ";
+            int t; if (!(std::cin >> t)) { std::cin.clear(); std::cin.ignore(10000, '\n'); break; }
+            if (t == 1) {
+                tree.preorder([](int v) { std::cout << v << " "; });
+            }
+            else if (t == 2) {
+                tree.inorder([](int v) { std::cout << v << " "; });
+            }
+            else {
+                tree.postorder([](int v) { std::cout << v << " "; });
+            }
+            std::cout << "\n";
+            std::cin.ignore(10000, '\n');
             break;
-        default:
-            std::cout << "Nieprawidlowy wybor !\n";
         }
-    }
-
-    std::cout << "Koniec programu !\n";
-    return 0;
-}
-
 
